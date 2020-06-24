3aa77092e78f12040bd439f0a13e131607e409ea
Imaad Syed <30536128+isyedjr@users.noreply.github.com>
2020-06-24 11:22:18 -0400

    Update Group5-CNN.py

--- Group5-CNN.py
+++ Group5-CNN.py
@@ -16,21 +16,29 @@ else:
   dev = torch.device("cpu")
 
 transform = transforms.Compose(torchvision.transforms.ToTensor())
-  
+
+# probably has to change
 dataPath = '/baseline methods/Self-Trans/LUNA/train'
+
 lRate = 0.001
- 
-class CNN(nn.modules):
+
+# model
+class CNN(nn.Module):
   # constructor
   def __init__(self):
     super(CNN, self).__init__()
-    self.conv1 = nn.Conv2d(3, 224, 5)
+    
+    # Convolution operations
+    self.conv1 = nn.Conv2d(1, 224, 5)
     self.pool = nn.MaxPool2d(3,3)
     self.conv2 = nn.Conv2d(224,224,3)
+    
+    # ANN operations
     self.fc1 = nn.Linear(224*224*3,120)
     self.fc2 = nn.Linear(120,84)
     self.fc3 = nn.Linear(84,2)
   
+  # ID sequence:
   # conv1 -> pool -> conv2 -> pool -> fc1 -> fc2 -> fc3
   def forward(self, run):
     run = self.pool(f.relu(self.conv1(run)))
@@ -42,4 +50,5 @@ class CNN(nn.modules):
     return run
  
 CNN1 = CNN()
+print("Model created!")
 
