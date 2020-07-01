048059778a2a2fb191e72bc6f4a9b3c5b505a3aa
Imaad Syed <30536128+isyedjr@users.noreply.github.com>
2020-06-30 23:29:02 -0400

    Update ExecutableFile.py

--- ExecutableFile.py
+++ ExecutableFile.py
@@ -7,19 +7,43 @@ from skimage import io, transform
 from PIL import Image
 import torch.nn.functional as f
 import torchvision.transforms as transforms
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+
+class runData():
+    def __init__(self, csvFile, transform = None):
+        self.transform = transform
+        self.CT = pd.read_csv(csvFile)
+
+    def __len__(self):
+        return len(self.CT)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        imgName = os.path.join('dataset2/', self.CT.iloc[idx,1])
+        img = Image.open(imgName).convert('L')
+        cS = self.CT.iloc[idx,2]
+
+        if self.transform:
+            img = self.transform(img)
+
+        return (img, cS)
+
 
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
         # Convolution operations - test other values for 32 and 64
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, 3, padding = 1)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, 3)
 
         # ANN operations - test other values for 1000 and 100
-        self.fc1 = nn.Linear(55 * 55 * 64, 1000)  # 64 should match above's value (2nd arg in conv2)
-        self.fc2 = nn.Linear(1000, 100)
+        self.fc1 = nn.Linear(55 * 55 * 64, 100)  # 64 should match above's value (2nd arg in conv2)
+        self.fc2 = nn.Linear(100, 100)
         self.fc3 = nn.Linear(100, 2)
 
     # ID sequence:
@@ -42,7 +66,7 @@ exec = CNN()
 exec.load_state_dict(torch.load('model.pt'))
 #exec.eval()
 
-transform1 = transforms.Compose([transforms.Resize((224, 224)), transforms.RandomRotation((-20, 20)), transforms.RandomAffine(0, translate=None, scale=[0.7, 1.3], shear=None, resample=False, fillcolor=0), transforms.ToTensor()])
+transform1 = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
 
 run = True
 
@@ -53,8 +77,18 @@ while (run):
         run = False
     else:
         print(id_img)
+        dataRun = open("run.csv", "x")
+        dataRun.write("id,img,status")
+        dataRun.write("\n")
+        dataRun.write("1,"+ id_img + ",unknown")
+        dataRun.close()
+        runner = runData('run.csv', transform = transform1)
+        runLoader = torch.utils.data.DataLoader(runner, batch_size = 1, shuffle=True)
         plt.figure()
         plt.imshow(io.imread(os.path.join('dataset2', id_img)))
         plt.show()
-        output = exec(transform1(Image.open(os.path.join('dataset2', id_img)).convert('RGB')))
-        print(output)
+        with torch.no_grad():
+            for i, (img, status) in enumerate(runLoader):
+                output = exec(img)
+                print(output)
+        os.remove("run.csv")
