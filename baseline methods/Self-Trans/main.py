4d85b5468ac3521cb8455e8ba56f9e1e5e446e8d
Carlos-Armendariz <carlosarmendariz57@gmail.com>
2020-07-07 14:49:54 -0400

    Added save location command line argument, fixed issues with previously committing wrong version

--- baseline methods/Self-Trans/main.py
+++ baseline methods/Self-Trans/main.py
@@ -9,7 +9,9 @@
 # In[4]:
 
 
-get_ipython().system("python main_moco.py  -a densenet169   --data ../../Images-processed --lr 0.015   --batch-size 64   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed 4 --world-size 1 --rank 0 --resume save_model_dense/checkpoint_luna.tar --epochs 40")
+get_ipython().system("python main_moco.py  -a densenet169   --data ../../Images-processed --lr 0.015   --batch-size "
+                     "32   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed 1 --world-size 1 --rank 0 "
+                     " --epochs 5")
 # --resume save_model_dense/checkpoint_covid.pth.tar
 #--data ../../Images-processed
 
