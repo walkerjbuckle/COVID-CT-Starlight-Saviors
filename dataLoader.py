1ea0bea8b80ead8eab1253c7d57ba4f56a14955f
Imaad Syed <30536128+isyedjr@users.noreply.github.com>
2020-07-07 13:59:12 -0400

    Update dataLoader.py

--- dataLoader.py
+++ dataLoader.py
@@ -6,6 +6,7 @@ import numpy as np
 from PIL import Image
 from skimage import io, transform
 import matplotlib.pyplot as plt
+from skimage.transform import rotate, AffineTransform, warp
 
 class data(Dataset):
     # construct dataset
@@ -27,6 +28,39 @@ class data(Dataset):
         img_Name = os.path.join(self.root, self.CT.iloc[idx,1])
         img = Image.open(img_Name).convert('L')
         cS = self.CT.iloc[idx,3]
+
+        if self.transform:
+            img = self.transform(img)
+	
+        return (img, cS)
+
+class EngineeredData(Dataset):
+	# construct dataset
+    def __init__(self, csvFile, root, transform = None, op):
+        self.transform = transform
+        self.root = root
+        self.CT = pd.read_csv(csvFile)
+	self.op = op
+    
+    # dataset length
+    def __len__(self):
+        return len(self.CT)
+    
+    # indexing
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+	# image
+        img_Name = os.path.join(self.root, self.CT.iloc[idx,1])
+        img = Image.open(img_Name).convert('L')
+        cS = self.CT.iloc[idx,3]
+	if op == 1:
+		img = np.flipud(img)
+	elif op == 2:
+		seed(1)
+		a = (random()) * 300
+		img = rotate(img, angle=a, mode = 'wrap')
 
         if self.transform:
             img = self.transform(img)
