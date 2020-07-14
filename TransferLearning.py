d398fbde2fd566264548bbcf536d4201d76f5ece
aabansyed11 <67119999+aabansyed11@users.noreply.github.com>
2020-07-14 13:05:57 -0400

    Update TransferLearning.py

--- TransferLearning.py
+++ TransferLearning.py
@@ -17,5 +17,25 @@ plt.ion()
 #Finetuning
 model_ft = models.resnet18(pretrained=True)
 num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 2)
+
+model_ft = modelt._ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 #Fixed Feature Extracter
+model_conv = torchvision.models.resnet18(pretrained=True)
+for param in model_conv.parameters():
+  param.requires_grad = False
+  
+ model_conv = model_conv.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
