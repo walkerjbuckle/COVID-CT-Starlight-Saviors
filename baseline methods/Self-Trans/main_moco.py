09d2a865565e95be7339d1d2e5934e38c210a464
Carlos-Armendariz <carlosarmendariz57@gmail.com>
2020-07-22 19:00:08 -0400

    Modified to work with training API script

--- baseline methods/Self-Trans/main_moco.py
+++ baseline methods/Self-Trans/main_moco.py
@@ -40,9 +40,7 @@ model_names = {
         'efficientnet-b0': Efficientnet.efficientnetb0
     }
 
-# model_names = sorted(name for name in models.__dict__
-#     if name.islower() and not name.startswith("__")
-#     and callable(models.__dict__[name]))
+
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('--data',  default='LUNA', type=str,
@@ -56,9 +54,9 @@ parser.add_argument('-j', '--workers', default=32, type=int, metavar='N',
                     help='number of data loading workers (default: 32)')
 parser.add_argument('--epochs', default=1000, type=int, metavar='N',
                     help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=128, type=int,
+parser.add_argument('-b', '--batch_size', default=128, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
@@ -69,54 +67,54 @@ parser.add_argument('--schedule', default=[120, 160], nargs='*', type=int,
                     help='learning rate schedule (when to drop lr by 10x)')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum of SGD solver')
-parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+parser.add_argument('--wd', '--weight_decay', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)',
                     dest='weight_decay')
-parser.add_argument('-p', '--print-freq', default=10, type=int,
+parser.add_argument('-p', '--print_freq', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
-parser.add_argument('--world-size', default=1, type=int,
+parser.add_argument('--world_size', default=1, type=int,
                     help='number of nodes for distributed training')
 parser.add_argument('--rank', default=0, type=int,
                     help='node rank for distributed training')
-parser.add_argument('--dist-url', default='tcp://localhost:10001', type=str,
+parser.add_argument('--dist_url', default='tcp://localhost:10001', type=str,
                     help='url used to set up distributed training')
-parser.add_argument('--dist-backend', default='nccl', type=str,
+parser.add_argument('--dist_backend', default='nccl', type=str,
                     help='distributed backend')
 parser.add_argument('--seed', default=None, type=int,
                     help='seed for initializing training. ')
 parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
-parser.add_argument('--multiprocessing-distributed', default=True,
+parser.add_argument('--multiprocessing_distributed', default=True,
                     help='Use multi-processing distributed training to launch '
                          'N processes per node, which has N GPUs. This is the '
                          'fastest way to use PyTorch for either single node or '
                          'multi node data parallel training')
 
-parser.add_argument('--save-epoch',default=40,type=int)
-parser.add_argument('--save-path',default='LUNA_resnet50_128_imagenet',type=str)
+parser.add_argument('--save_epoch',default=40,type=int)
+parser.add_argument('--save_path',default='LUNA_resnet50_128_imagenet',type=str)
 
 # moco specific configs:
-parser.add_argument('--moco-dim', default=128, type=int,
+parser.add_argument('--moco_dim', default=128, type=int,
                     help='feature dimension (default: 128)')
-parser.add_argument('--moco-k', default=128, type=int,
+parser.add_argument('--moco_k', default=128, type=int,
                     help='queue size; number of negative keys (default: 65536)')
-parser.add_argument('--moco-m', default=0.999, type=float,
+parser.add_argument('--moco_m', default=0.999, type=float,
                     help='moco momentum of updating key encoder (default: 0.999)')
-parser.add_argument('--moco-t', default=0.07, type=float,
+parser.add_argument('--moco_t', default=0.07, type=float,
                     help='softmax temperature (default: 0.07)')
 
 # options for moco v2
 parser.add_argument('--mlp', default=True,
                     help='use mlp head')
-parser.add_argument('--aug-plus', default=True,
+parser.add_argument('--aug_plus', default=True,
                     help='use moco v2 data augmentation')
 parser.add_argument('--cos', default=True,
                     help='use cosine lr schedule')
 
-def main():
-    args = parser.parse_args()
+def main(args = None):
+    args = parser.parse_args(args)
 
     if args.seed is not None:
         random.seed(args.seed)
